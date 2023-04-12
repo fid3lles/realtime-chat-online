dd03e3ca786845c3b55242122083c7acb295f234
fid3lles <gabrielfidellesc@hotmail.com>
2023-04-12 15:51:25 -0300

    [FEATURE] Chat com pessoas

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 import express from 'express';
 import http from 'http';
 import socketio from 'socket.io';
+import path from 'path';
 
 const app = express();
 const server = http.createServer(app);
 const sockets = socketio(server);
+const __dirname = path.resolve();
 
 var messages = [];
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 sockets.emit();
 
 sockets.on('connection', (socket) => {
     const userId = socket.id;
     console.log(`> User connected: ${userId}`);
+    socket.broadcast.emit('new-user', userId);
 
     socket.on('disconnect', () => {
         console.log(`> User disconnected: ${userId}`);
@@ -22,6 +25,7 @@ sockets.on('connection', (socket) => {
 
     socket.on('send-message', data => {
         messages.push(data);
+        console.log("Mensagem recebida:", data);
         socket.broadcast.emit('receveid-message', data);
     });
 });
